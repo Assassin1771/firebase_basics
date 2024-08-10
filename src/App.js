17b2c8f5433241525d12fadec9a2cb37be4987de
Tushar Garg <124759513+tushar-procol@users.noreply.github.com>
2024-08-10 15:21:19 +0530

    install and connect firebase with realtime db (#1)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { firebaseApp } from "./firebase";
+import { getDatabase, ref, set } from "firebase/database";
+
+const firebaseDb = getDatabase(firebaseApp);
 
 const App = () => {
-  return <div>FIREBASE REACT APP</div>;
+  const setData = (id) => {
+    set(ref(firebaseDb, "users/" + id), {
+      name: "Tushar Garg",
+      age: 23,
+    });
+  };
+
+  return (
+    <div>
+      <div>FIREBASE REACT APP</div>
+      <button onClick={() => setData(1)}>set data in realtime db</button>
+    </div>
+  );
 };
 
 export default App;
